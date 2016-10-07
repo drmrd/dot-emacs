19046439f87eb2642e3896accdf64a69f99a3242
Dan Moore <hello@danielmoore.xyz>
2016-10-07 09:46:27 -0400

    Remove debug-on-error from customizations
    Not sure what package set this in the customizations buffer (or if I did
    unintentionally), but that's a setting I'd much rather enable by hand!

--- customizations.el
+++ customizations.el
@@ -197,13 +197,10 @@
        ("note"))))))
  '(bmkp-last-as-first-bookmark-file "~/.emacs.d/bookmarks")
  '(column-number-mode t)
- '(custom-enabled-themes (quote (cyberpunk)))
  '(custom-safe-themes
    (quote
     ("e64111716b1c8c82638796667c2c03466fde37e69cada5f6b640c16f1f4e97df" "427fed191e7a766152e59ef0e2904283f436dbbe259b9ccc04989f3acde50a55" "84d2f9eeb3f82d619ca4bfffe5f157282f4779732f48a5ac1484d94d5ff5b279" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "f9574c9ede3f64d57b3aa9b9cef621d54e2e503f4d75d8613cbcc4ca1c962c21" "1bacdd5d24f187f273f488a23c977f26452dffbc82d4ac57250aa041f14159da" "71ecffba18621354a1be303687f33b84788e13f40141580fa81e7840752d31bf" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "959a77d21e6f15c5c63d360da73281fdc40db3e9f94e310fc1e8213f665d0278" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" default)))
- '(debug-on-error t)
  '(delete-selection-mode t)
- '(fci-rule-color "#383838")
  '(fixmee-cache-refresh-interval 5)
  '(fixmee-notice-regexp
    "\\(@@@+\\|\\_<\\(?:[Tt][Oo][Dd][Oo]+\\|[Ff][Ii][Xx][Mm][Ee]+\\|[Cc][Ii][Tt][Ee][Mm][Ee]+\\|[Mm][Aa][Yy][Bb][Ee]+\\|XXX+\\|[Ss][Tt][Aa][Rr][Tt][Ee][Dd]+\\)\\)\\(?:[/:?!. 	
